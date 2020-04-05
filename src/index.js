203afcebe1f5034e2fadf70ce99a659937004739
Donnie D'Amato <fauxserious@gmail.com>
2020-04-05 16:30:23 -0400

    Update index page, parse @link tags

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const Page = require('../lib/page.js');
 module.exports = new Page('Welcome!', () => {
   /**
-   * Comments only in here, no code!
+   * This website aims to highlight the power of regular JavaScript when used in the browser. No frameworks; just native DOM APIs for modern browsers. {@link https://github.com/ddamato/dom-tricks|Edit this code for this website on Github}
    */
 }).related([
-  './class-attribute.js'
+  './class-attribute.js',
+  './click-outside',
+  './create-element.js',
+  './css-styles.js',
+  './event-handler.js',
 ]);
